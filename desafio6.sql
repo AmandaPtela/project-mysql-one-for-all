c07795fb80f8e92671d704871403effec2e562d1
Amanda Portela <mandaptela@gmail.com>
2022-09-28 16:56:55 -0300

    Desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
-SELECT  if(MIN(valor_plano) = TRUE, '0.00', 0.00) AS faturamento_minimo,
+SELECT  MIN(valor_plano) AS faturamento_minimo,
 		    MAX(valor_plano) AS faturamento_maximo,
         ROUND(AVG(valor_plano), 2) AS faturamento_medio,
         ROUND(SUM(valor_plano), 2) AS faturamento_total
